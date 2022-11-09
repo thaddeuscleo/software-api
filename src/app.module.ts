53732b229eb5ccb487b3c8c6ef3ec1da6050ec9b
Thaddeus Cleo <thaddeuscleo@gmail.com>
2022-11-09 13:20:02 +0700

    remove app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { SoftwaresModule } from './softwares/softwares.module';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { PrismaService } from './prisma-service/prisma.service';
@@ -19,7 +17,6 @@ import { PrismaService } from './prisma-service/prisma.service';
     }),
     SoftwaresModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [PrismaService],
 })
 export class AppModule {}
