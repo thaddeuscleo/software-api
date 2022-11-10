11407a706c0680ec91d490cb887eaeec6ccd0489
Thaddeus Cleo <thaddeuscleo@gmail.com>
2022-11-10 19:06:17 +0700

    feat: add config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,29 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { SoftwaresModule } from './softwares/softwares.module';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
-import { PrismaService } from './prisma-service/prisma.service';
 import { RoomsModule } from './rooms/rooms.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      sortSchema: true,
-      // plugins: [ApolloServerPluginLandingPageLocalDefault()],
-      // playground: false,
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => {
+        const isApolloSandBox = config.get<string>('APOLLO_SANDBOX') === 'true'; 
+        const isPlayground = config.get<string>('PLAYGROUND') === 'true' && !isApolloSandBox;
+        const plugin = isApolloSandBox ? ApolloServerPluginLandingPageLocalDefault() : {};
+
+        return {
+          autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+          sortSchema: true,
+          plugins: [plugin],
+          playground: isPlayground,
+        };
+      },
+      inject: [ConfigService],
     }),
+    ConfigModule.forRoot(),
     SoftwaresModule,
     RoomsModule,
   ],
